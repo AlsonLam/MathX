6c93870dc01e26b105db68f6c454519db1c12ebd
AlsonLam <alson_lam_hong_yao@s2017.sst.edu.sg>
2018-08-09 11:56:24 +0800

    added items

--- app/src/main/java/com/example/alson/mathx/Notes.java
+++ app/src/main/java/com/example/alson/mathx/Notes.java
@@ -1,18 +1,23 @@
 package com.example.alson.mathx;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class Notes extends AppCompatActivity
 implements LoaderManager.LoaderCallbacks<Cursor>
@@ -23,13 +28,10 @@ implements LoaderManager.LoaderCallbacks<Cursor>
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        insertNote("New note");
+       String[] from = {DBOpenHelper.NOTE_TEXT};
+        int[] to = {R.id.tvNote};
 
-
-        String[] from = {DBOpenHelper.NOTE_TEXT};
-        int[] to = {android.R.id.text1};
-
-        cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,null,from,to,0);
+        cursorAdapter = new SimpleCursorAdapter(this, R.layout.note_list_item, null, from, to,0);
 
         ListView list = findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
@@ -62,8 +64,37 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 
         return super.onOptionsItemSelected(item);
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private void deleteAllNotes() {
+
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            //Insert Data management code here
+                           getContentResolver().delete(
+                                   NotesProvider.CONTENT_URI, null, null
+                           );
+                           restartLoader();
+
+                            Toast.makeText(Notes.this,
+                                    getString(R.string.all_deleted),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
     }
 
     private void insertSampleData(){
